21c1a4b21b1dc7f676863f04eb8fa9ae8d7ac132
Muhammad Amal MY <MAmal@kerrylogistics.com>
2019-03-01 12:44:09 +0800

    Create CarsController, Insert Connection and DbFactories Strings,Create view of Create, List, Update and Delete, and improve UI

--- CarRentalMS/Controllers/HomeController.cs
+++ CarRentalMS/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace CarRentalMS.Controllers
 {
@@ -10,13 +6,6 @@ namespace CarRentalMS.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
