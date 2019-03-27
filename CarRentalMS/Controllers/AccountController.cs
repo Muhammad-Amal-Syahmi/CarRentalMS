bbb49dbdbf6b67cd9770f633a4e6b787d677afe6
Muhammad Amal MY <MAmal@kerrylogistics.com>
2019-03-27 17:02:01 +0800

    get username by email and login using username

--- CarRentalMS/Controllers/AccountController.cs
+++ CarRentalMS/Controllers/AccountController.cs
@@ -73,7 +73,16 @@ namespace CarRentalMS.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var user = await UserManager.FindByEmailAsync(model.Email);
+
+            if(user == null)
+            {
+                TempData["msgFailed"] = "Login";
+                ModelState.AddModelError("", "Email not found. Are you sure you already registered?");
+                return View(model);
+            }
+
+            var result = await SignInManager.PasswordSignInAsync(user.UserName, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
